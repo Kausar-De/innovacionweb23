e748385cd4ee556e1794f1bddb56aa445ecaf781
Kausar De <kde48business@gmail.com>
2023-03-09 11:24:28 +0530

    2023 Changes

--- userint/forms.py
+++ userint/forms.py
@@ -10,6 +10,9 @@ class ParticipantForm(ModelForm):
     class Meta:
         model = Participant
         exclude = ['participant', 'created_date']
+        widgets = {
+            'levelchoice': forms.RadioSelect(),
+        }
         labels = {
             'teamname': _('Team Name'),
             'teamlead': _('Name of Team Lead'),
@@ -18,7 +21,9 @@ class ParticipantForm(ModelForm):
             'member2': _('Name of Member 2'),
             'member3': _('Name of Member 3'),
             'member4': _('Name of Member 4'),
+            'levelchoice': _('Difficulty Level'),
             'repolink': _('Link to GitHub Repository'),
             'livelink': _('Link to Live Deployment'),
+            'videolink': _('Link to Submission Video'),
             'created_date': _('Submission Date & Time'),
         }
--- userint/models.py
+++ userint/models.py
@@ -6,6 +6,11 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class Participant(models.Model):
+    LEVEL_CHOICES = (
+        ('Beginner', 'Beginner'),
+        ('Advanced', 'Advanced'),
+    )
+
     participant = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete = models.CASCADE, null = True)
     teamname = models.CharField(max_length = 200, null = True)
     teamlead = models.CharField(max_length = 200, null = True)
@@ -14,8 +19,10 @@ class Participant(models.Model):
     member2 = models.CharField(max_length = 200, null = True)
     member3 = models.CharField(max_length = 200, null = True)
     member4 = models.CharField(max_length = 200, null = True)
+    levelchoice = models.CharField(choices = LEVEL_CHOICES, max_length = 200, null = True, default = LEVEL_CHOICES[0][0])
     repolink = models.CharField(max_length = 500, null = True, unique = True)
     livelink = models.CharField(max_length = 500, null = True, unique = True)
+    videolink = models.CharField(max_length = 500, null = True, unique = True)
     created_date = models.DateTimeField(default = timezone.now, null = True)
 
     def __str__(self):
