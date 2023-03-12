380f53f92e59c01f6a621a3c6b488d9b90f452cb
Kausar De <kde48business@gmail.com>
2023-03-12 11:20:24 +0530

    Revert "Added excel export"
    This reverts commit 51c311e4e0b7e40b78aa9e73b0eed29b56e98bf4.

--- userint/admin.py
+++ userint/admin.py
@@ -1,10 +1,6 @@
 from django.contrib import admin
 from .models import *
-from import_export.admin import ExportActionMixin
 
 # Register your models here.
 
-class ParticipantAdmin(ExportActionMixin, admin.ModelAdmin):
-    list_display = ('participant', 'teamname', 'teamlead', 'leadphone', 'leademail', 'member2', 'member3', 'member4', 'levelchoice', 'repolink', 'livelink', 'codingvideolink', 'videolink', 'created_date')
-
-admin.site.register(Participant, ParticipantAdmin)
+admin.site.register(Participant)
